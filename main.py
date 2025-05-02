f145e1b064ab83eb8bcf1912e2b33a24d6fa4f32
zbotvr <imprettycoolforschool@gmail.com>
2025-05-02 16:15:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def home():
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>DEATH-TON Proxy</title>
+        <title>DEATH-TON</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
           tailwind.config = {
@@ -32,7 +32,7 @@ def home():
             <h1 class="text-3xl font-bold mb-4">School Proxy Unblocker</h1>
             <p class="mb-6 text-gray-400" id="desc">Bypass anything from your school. Stay sneaky.</p>
             <form action="/go" method="get" class="flex gap-2 justify-center">
-                <input name="url" placeholder="Enter URL (e.g. https://example.com)"
+                <input name="url" placeholder="Enter URL (e.g. https://google.com)"
                     class="px-4 py-2 rounded bg-gray-700 text-white w-80 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     required id="input">
                 <button type="submit"
